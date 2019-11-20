07e1e4c33abd403bed50b8562d74ec20493cd41b
jball16 <jball16@vols.utk.edu>
2019-11-20 17:06:04 +0000

    fixed ast maker

--- create_ast.py
+++ create_ast.py
@@ -1,6 +1,8 @@
 from pymongo import MongoClient
 from tqdm import tqdm
 import ast
+from bson.objectid import ObjectId
+import math
 
 print("Connecting to db...")
 client = MongoClient('da1.eecs.utk.edu')
@@ -9,16 +11,18 @@ col = db['SOdata']
 
 docs = list(col.find({}))
 for doc in tqdm(docs):
-    removed = False
     all_blocks = doc['code_and_ast']
     for i, each in enumerate(all_blocks):
+        #####################################
         if not isinstance(each, dict):
-            removed = True
             col.remove({"_id": doc['_id']})
             break
-        if each['compilation_message'] is None or each['compilation_message'] == 'nan':
-            print(each['compilation_message'])
-        if doc['language'] == 'python-3.x' and (each['compilation_message'] is None or each['error_type'] is None or each['ast'] is None):
+        #####################################
+        cmp_str = 'code_and_ast.'+str(i)+'.compilation_message'
+        err_str = 'code_and_ast.'+str(i)+'.error_type'
+        ast_str = 'code_and_ast.'+str(i)+'.ast'
+
+        if doc['language'] == 'python-3.x' and (not isinstance(each['compilation_message'], str) or not isinstance(each['error_type'], str) or not isinstance(each['ast'], str)):
             code = each['code']
             # try to compile the code
             message = ''
@@ -33,12 +37,9 @@ for doc in tqdm(docs):
             if message == '':
                 #print("\nCompilation successful!")
                 #print("#_______________________________________________________________________________#")
-                #col.update_one({'code_and_ast': {'code': code}}, {"$set": {'code': {'compilation_message': 'successful'}}})
-                #col.update_one({'code_and_ast': {'code': code}}, {"$set": {'code': {'error_type': 'none'}}})
-                #col.update_one({'code_and_ast': {'code': code}}, {"$set": {'code': {'ast': tree_dump}}})
-                all_blocks[i]['compilation_message'] = 'successful' 
-                all_blocks[i]['error_type'] = 'none' 
-                all_blocks[i]['ast'] = tree_dump 
+                col.update({'_id': ObjectId(doc['_id']), 'code_and_ast.code': code}, {"$set": {cmp_str: 'successful'}})
+                col.update({'_id': ObjectId(doc['_id']), 'code_and_ast.code': code}, {"$set": {err_str: 'none'}})
+                col.update({'_id': ObjectId(doc['_id']), 'code_and_ast.code': code}, {"$set": {ast_str: tree_dump}})
             else:
                 if err == "ModuleNotFoundError":
                     fw = open('NEEDED_LIBRARIES', 'a')
@@ -53,13 +54,9 @@ for doc in tqdm(docs):
                     continue
                 #print("\nCompilation failed!")
                 #print("#_______________________________________________________________________________#")
-                #col.update_one({'code_and_ast': {'code': code}}, {"$set": {'code': {'compilation_message': str(message)}}})
-                #col.update_one({'code_and_ast': {'code': code}}, {"$set": {'code': {'error_type': err}}})
-                #col.update_one({'code_and_ast': {'code': code}}, {"$set": {'code': {'ast': 'none'}}})
-                all_blocks[i]['compilation_message'] = str(message) 
-                all_blocks[i]['error_type'] = err
-                all_blocks[i]['ast'] = 'none'
+                col.update({'_id': ObjectId(doc['_id']), 'code_and_ast.code': code}, {"$set": {cmp_str: str(message)}})
+                col.update({'_id': ObjectId(doc['_id']), 'code_and_ast.code': code}, {"$set": {err_str: err}})
+                col.update({'_id': ObjectId(doc['_id']), 'code_and_ast.code': code}, {"$set": {ast_str : 'none'}})
             #print()
             #print()
-    if not removed:
-        col.update_one({'code_and_ast': doc['code_and_ast']}, {'$set': {'code_and_ast': all_blocks}})
+
